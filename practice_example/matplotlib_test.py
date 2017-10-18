ef5d5ef3190fd0cde956dea6b8ed9cc6a14485ae
cklin <cklin@itri.org.tw>
2017-10-18 17:01:46 +0800

    python material update

---
+++ practice_example/matplotlib_test.py
@@ -0,0 +1,397 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+
+
+#1 基本用法 
+x = np.linspace(-1, 1, 50)
+y = 2*x + 1
+# y = x**2
+plt.plot(x, y)
+plt.show()
+
+
+#2 figure 圖像
+x = np.linspace(-3, 3, 50)
+y1 = 2*x + 1
+y2 = x**2
+
+plt.figure()
+plt.plot(x, y1)
+
+
+plt.figure(num=3, figsize=(8, 5),)
+plt.plot(x, y2)
+# plot the second curve in this figure with certain parameters
+plt.plot(x, y1, color='red', linewidth=1.0, linestyle='--')
+plt.show()
+
+
+#3 设置坐标轴 - part1
+
+x = np.linspace(-3, 3, 50)
+y1 = 2*x + 1
+y2 = x**2
+
+plt.figure()
+plt.plot(x, y2)
+# plot the second curve in this figure with certain parameters
+plt.plot(x, y1, color='red', linewidth=1.0, linestyle='--')
+
+# set x, y limit
+plt.xlim((-1,2))
+plt.ylim((-3,3))
+
+# set x, y label
+plt.xlabel("I am x")
+plt.ylabel("I am y")
+
+# set new sticks
+new_ticks = np.linspace(-1, 2, 5)
+print(new_ticks)
+plt.xticks(new_ticks)
+# set tick labels
+plt.yticks([-2, -1.8, -1, 1.22, 3],
+           [r'$really\ bad$', r'$bad\ \alpha$', r'$normal$', r'$good$', r'$really\ good$'])
+plt.show()
+
+
+#4 设置坐标轴 - part2
+x = np.linspace(-3, 3, 50)
+y1 = 2*x + 1
+y2 = x**2
+
+plt.figure()
+plt.plot(x, y2)
+# plot the second curve in this figure with certain parameters
+plt.plot(x, y1, color='red', linewidth=1.0, linestyle='--')
+
+# set x, y limit
+plt.xlim((-1,2))
+plt.ylim((-3,3))
+
+# set x, y label
+plt.xlabel("I am x")
+plt.ylabel("I am y")
+
+# set new sticks
+new_ticks = np.linspace(-1, 2, 5)
+print(new_ticks)
+plt.xticks(new_ticks)
+# set tick labels
+plt.yticks([-2, -1.8, -1, 1.22, 3],
+           [r'$really\ bad$', r'$bad$', r'$normal$', r'$good$', r'$really\ good$'])
+# to use '$ $' for math text and nice looking, e.g. '$\pi$'
+
+# gca = 'get current axis'
+ax = plt.gca()
+ax.spines['right'].set_color('none')
+ax.spines['top'].set_color('none')
+
+ax.xaxis.set_ticks_position('bottom')
+# ACCEPTS: [ 'top' | 'bottom' | 'both' | 'default' | 'none' ]
+
+ax.spines['bottom'].set_position(('data', 0))
+# the 1st is in 'outward' | 'axes' | 'data'
+# axes: percentage of y axis
+# data: depend on y data
+
+ax.yaxis.set_ticks_position('left')
+# ACCEPTS: [ 'left' | 'right' | 'both' | 'default' | 'none' ]
+
+ax.spines['left'].set_position(('data',0))
+plt.show()
+
+
+#5  Legend 图例
+x = np.linspace(-3, 3, 50)
+y1 = 2*x + 1
+y2 = x**2
+
+plt.figure()
+
+# set x, y limit
+plt.xlim((-1,2))
+plt.ylim((-3,3))
+
+# set x, y label
+plt.xlabel("I am x")
+plt.ylabel("I am y")
+
+# set new sticks
+new_ticks = np.linspace(-1, 2, 5)
+print(new_ticks)
+plt.xticks(new_ticks)
+# set tick labels
+plt.yticks([-2, -1.8, -1, 1.22, 3],
+           [r'$really\ bad$', r'$bad$', r'$normal$', r'$good$', r'$really\ good$'])
+# to use '$ $' for math text and nice looking, e.g. '$\pi$'
+
+l1, = plt.plot(x, y2, label='up')
+l2, =plt.plot(x, y1, color='red', linewidth=1.0, linestyle='--', label='down')
+plt.legend(loc='upper right')
+# plt.legend(handles=[l1, l2], labels=['up', 'down'],  loc='best')
+# the "," is very important in here l1, = plt... and l2, = plt... for this step
+"""legend( handles=(line1, line2, line3),
+           labels=('label1', 'label2', 'label3'),
+           'upper right')
+    The *loc* location codes are::
+          'best' : 0,          (currently not supported for figure legends)
+          'upper right'  : 1,
+          'upper left'   : 2,
+          'lower left'   : 3,
+          'lower right'  : 4,
+          'right'        : 5,
+          'center left'  : 6,
+          'center right' : 7,
+          'lower center' : 8,
+          'upper center' : 9,
+          'center'       : 10,"""
+plt.show()
+
+
+#6 Annotation 标注
+x = np.linspace(-3, 3, 50)
+y = 2*x + 1
+
+plt.figure(num=1, figsize=(8, 5),)
+plt.plot(x, y,)
+#plt.scatter(x,y)
+
+ax = plt.gca()
+ax.spines['right'].set_color('none')
+ax.spines['top'].set_color('none')
+ax.xaxis.set_ticks_position('bottom')
+ax.spines['bottom'].set_position(('data', 0))
+ax.yaxis.set_ticks_position('left')
+ax.spines['left'].set_position(('data', 0))
+
+x0 = 1
+y0 = 2*x0 + 1
+#plt.scatter(x0,y0)
+plt.scatter(x0,y0,s=50,color='b')
+#plt.scatter(x0,y0,s=100,color='r')
+
+plt.plot([x0, x0,], [0, y0,], 'k--', linewidth=2.5)
+plt.scatter([x0, ], [y0, ], s=50, color='b')
+
+# method 1:
+#####################
+plt.annotate(r'$2x+1=%s$' % y0, xy=(x0, y0), xycoords='data', xytext=(+30, -30),
+             textcoords='offset points', fontsize=16,
+             arrowprops=dict(arrowstyle='->', connectionstyle="arc3,rad=.2"))
+
+# method 2:
+########################
+plt.text(-3.7, 3, r'$This\ is\ the\ some\ text. \mu\ \sigma_i\ \alpha_t$',
+         fontdict={'size': 18, 'color': 'r'})
+
+plt.show()
+
+#7 tick 能见度
+x = np.linspace(-3, 3, 50)
+y = 0.1*x
+
+plt.figure()
+plt.plot(x, y, linewidth=10)
+plt.ylim(-2, 2)
+ax = plt.gca()
+ax.spines['right'].set_color('none')
+ax.spines['top'].set_color('none')
+ax.spines['top'].set_color('none')
+ax.xaxis.set_ticks_position('bottom')
+ax.spines['bottom'].set_position(('data', 0))
+ax.yaxis.set_ticks_position('left')
+ax.spines['left'].set_position(('data', 0))
+
+
+for label in ax.get_xticklabels() + ax.get_yticklabels():
+    label.set_fontsize(12)
+    label.set_bbox(dict(facecolor='white', edgecolor='None', alpha=0.1))
+plt.show()
+
+#8 scatter 散点图
+
+mu, sigma = 1, 0.1 # mean and standard deviation
+s = np.random.normal(mu, sigma, 1000)
+
+count, bins, ignored = plt.hist(s, 30, normed=True)
+plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) * np.exp( - (bins - mu)**2 / (2 * sigma**2) ),
+          linewidth=2, color='r')
+plt.show()
+
+
+n = 1024    # data size
+X = np.random.normal(0, 1, n)
+Y = np.random.normal(0, 1, n)
+T = np.arctan2(Y, X)    # for color later on
+
+plt.scatter(X, Y, s=75, c=T, alpha=.5)
+
+plt.xlim(-1.5, 1.5)
+plt.xticks(())  # ignore xticks
+plt.ylim(-1.5, 1.5)
+plt.yticks(())  # ignore yticks
+plt.show()
+
+#9 bar 柱状图
+
+n = 12
+X = np.arange(n)
+Y1 = (1 - X / float(n)) * np.random.uniform(0.5, 1.0, n)
+Y2 = (1 - X / float(n)) * np.random.uniform(0.5, 1.0, n)
+
+plt.bar(X, +Y1, facecolor='#9999ff', edgecolor='white')
+plt.bar(X, -Y2, facecolor='#ff9999', edgecolor='white')
+
+for x, y in zip(X, Y1):
+    # ha: horizontal alignment
+    # va: vertical alignment
+    plt.text(x + 0.4, y + 0.05, '%.2f' % y, ha='center', va='bottom')
+
+for x, y in zip(X, Y2):
+    # ha: horizontal alignment
+    # va: vertical alignment
+    plt.text(x + 0.4, -y - 0.05, '%.2f' % y, ha='center', va='top')
+
+plt.xlim(-.5, n)
+plt.xticks(())
+plt.ylim(-1.25, 1.25)
+plt.yticks(())
+
+plt.show()
+
+#10 contours 等高线图
+
+def f(x,y):
+    # the height function
+    return (1 - x / 2 + x**5 + y**3) * np.exp(-x**2 -y**2)
+
+n = 256
+x = np.linspace(-3, 3, n)
+y = np.linspace(-3, 3, n)
+X,Y = np.meshgrid(x, y)
+
+# use plt.contourf to filling contours
+# X, Y and value for (X,Y) point
+plt.contourf(X, Y, f(X, Y), 10, alpha=.75, cmap=plt.cm.hot)
+
+# use plt.contour to add contour lines
+C = plt.contour(X, Y, f(X, Y), 10, colors='black', linewidth=.5)
+# adding label
+plt.clabel(C, inline=True, fontsize=10)
+
+plt.xticks(())
+plt.yticks(())
+plt.show()
+
+# 11 image 图片
+a = np.array([0.313660827978, 0.365348418405, 0.423733120134,
+              0.365348418405, 0.439599930621, 0.525083754405,
+              0.423733120134, 0.525083754405, 0.651536351379]).reshape(3,3)
+
+"""
+for the value of "interpolation", check this:
+http://matplotlib.org/examples/images_contours_and_fields/interpolation_methods.html
+for the value of "origin"= ['upper', 'lower'], check this:
+http://matplotlib.org/examples/pylab_examples/image_origin.html
+"""
+plt.imshow(a, interpolation='nearest', cmap='bone', origin='lower')
+plt.colorbar(shrink=.92)
+
+plt.xticks(())
+plt.yticks(())
+plt.show()
+
+
+# 12 3D 数据
+
+fig = plt.figure()
+ax = Axes3D(fig)
+# X, Y value
+X = np.arange(-4, 4, 0.25)
+Y = np.arange(-4, 4, 0.25)
+X, Y = np.meshgrid(X, Y)
+R = np.sqrt(X ** 2 + Y ** 2)
+# height value
+Z = np.sin(R)
+
+ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=plt.get_cmap('rainbow'))
+"""
+============= ================================================
+        Argument      Description
+        ============= ================================================
+        *X*, *Y*, *Z* Data values as 2D arrays
+        *rstride*     Array row stride (step size), defaults to 10
+        *cstride*     Array column stride (step size), defaults to 10
+        *color*       Color of the surface patches
+        *cmap*        A colormap for the surface patches.
+        *facecolors*  Face colors for the individual patches
+        *norm*        An instance of Normalize to map values to colors
+        *vmin*        Minimum value to map
+        *vmax*        Maximum value to map
+        *shade*       Whether to shade the facecolors
+        ============= ================================================
+"""
+
+# I think this is different from plt12_contours
+ax.contourf(X, Y, Z, zdir='z', offset=-2, cmap=plt.get_cmap('rainbow'))
+"""
+==========  ================================================
+        Argument    Description
+        ==========  ================================================
+        *X*, *Y*,   Data values as numpy.arrays
+        *Z*
+        *zdir*      The direction to use: x, y or z (default)
+        *offset*    If specified plot a projection of the filled contour
+                    on this position in plane normal to zdir
+        ==========  ================================================
+"""
+
+ax.set_zlim(-2, 2)
+
+plt.show()
+
+# 13  subplot 多合一显示
+
+# example 1:
+###############################
+plt.figure(figsize=(6, 4))
+# plt.subplot(n_rows, n_cols, plot_num)
+plt.subplot(2, 2, 1)
+plt.plot([0, 1], [0, 1])
+
+plt.subplot(222)
+plt.plot([0, 1], [0, 2])
+
+plt.subplot(223)
+plt.plot([0, 1], [0, 3])
+
+plt.subplot(224)
+plt.plot([0, 1], [0, 4])
+
+plt.tight_layout()
+
+# example 2:
+###############################
+plt.figure(figsize=(6, 4))
+# plt.subplot(n_rows, n_cols, plot_num)
+plt.subplot(2, 1, 1)
+# figure splits into 2 rows, 1 col, plot to the 1st sub-fig
+plt.plot([0, 1], [0, 1])
+
+plt.subplot(234)
+# figure splits into 2 rows, 3 col, plot to the 4th sub-fig
+plt.plot([0, 1], [0, 2])
+
+plt.subplot(235)
+# figure splits into 2 rows, 3 col, plot to the 5th sub-fig
+plt.plot([0, 1], [0, 3])
+
+plt.subplot(236)
+# figure splits into 2 rows, 3 col, plot to the 6th sub-fig
+plt.plot([0, 1], [0, 4])
+
+
+plt.tight_layout()
+plt.show()
