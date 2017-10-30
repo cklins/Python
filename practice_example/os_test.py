5e40b99e2c32511db5afa5cc62c3bf7fd9c2e8d8
C.K.Lin <cklin@itri.org.tw>
2017-10-30 23:20:37 +0800

    material update

--- practice_example/os_test.py
+++ practice_example/os_test.py
@@ -6,7 +6,7 @@ Created on Mon Oct 30 15:48:56 2017
 """
 
 import os
-import sys
+from datetime import datetime
 import shutil
 
 print(os.getcwd())
@@ -37,6 +37,18 @@ fileName='myfile.txt'
 if not os.path.exists(fileName):
     open(fileName,'w').close
 
+
+#Rename file
+os.rename('myfile.txt','ck.txt')
+os.rename('ck.txt','myfile.txt')
+
+#檢視檔案
+print(os.stat('myfile.txt'))
+mod_time=os.stat('myfile.txt').st_mtime
+
+#formate timestamp
+print(datetime.fromtimestamp(mod_time))
+
     
 #刪除檔案 
 fileName = 'myfile.txt'
@@ -47,13 +59,15 @@ else:
     print("{} not found!!".format(fileName))
     
 
-#刪除資料夾多個檔案    
+#刪除資料夾多個檔案  
+os.chdir('D:/')  
 os.makedirs('CKLINS/Folder1')
 os.chdir('CKLINS/Folder1')
 for i in range(0,10):
     fileName = 'file{}'.format(i)
     print(fileName)
     open(fileName,'w').close()
+os.chdir('D:/')     
     
 if os.path.isdir('D:/CKLINS/Folder1'):
     shutil.rmtree('D:/CKLINS/Folder1')  # remove dir and all contains
@@ -61,6 +75,31 @@ else:
     raise ValueError("file {} is not a file or dir.".format('D:/CKLINS/Folder1'))
     
     
+#os.walk 回傳一個由三個元素的tuple所組成的串列(資料夾名稱, 下一個資料夾串列, 本資料夾內所有的檔案串列)
+
+for dirpath, dirnames, filenames in os.walk('C:\\Users\\cklin'):
+    print('Current Path:', dirpath)
+    print('Directories:', dirnames)
+    print('Files:', filenames)
+    print()
+    
+#envrionment
+print(os.environ)
+print(os.environ.get('HOMEPATH'))
+
+filename = 'test.txt'
+file_path = os.environ.get('HOMEPATH') + filename
+print(file_path) #bad
+file_path = os.path.join(os.environ.get('HOMEPATH'),filename)
+print(file_path)
+
+
+print(os.path.basename('C:\\Java\\jdk1.7.0_79'))
+print(os.path.dirname('C:\\Java\\jdk1.7.0_79'))
+print(os.path.split('C:\\Java\\jdk1.7.0_79'))
+print(os.path.splitext('C:\\Java\\jdk1.7.0_79\\bin\\java.exe'))
+print(dir(os.path))
+
 
     
 
